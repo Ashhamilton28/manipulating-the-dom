59749ca37accdfad02e166be99a57ac106e9d62b
Ashley <Ashhamilton22888@gmail.com>
2023-02-23 14:19:28 -0500

    first commit

--- script.js
+++ script.js
@@ -7,24 +7,31 @@ let quotes = [
   `It don't matter if you win by an inch or a mile. Winning's winning.`
 ];
 
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
   // Random quote of the day generator
-  const randomQuote = function() {
+  const randomQuote = function () {
     document.querySelector('#quote-of-the-day').textContent = `"${quotes[Math.floor(Math.random() * quotes.length)]}"`;
   };
   randomQuote();
-  
+
   // Do all of your work inside the document.addEventListener  
 
   // Part 1
+  let mainTitle = document.getElementById("main-title")
+  mainTitle.innerHTML = "Welcome to the DOM Homepage"
+
 
 
   // Part 2
-
+  let theBody = document.querySelector('body')
+  theBody.style.backgroundColor = '#3A1078'
 
   // Part 3
-
-
+  // let domsFavoriteThings = document.querySelector('ul.last-child')
+  let domsFavoriteThings = document.getElementById('favorite-things');
+ 
+  domsFavoriteThings.removeChild(domsFavoriteThings.lastElementChild)
+ console.log(domsFavoriteThings)
   // Part 4
 
 
