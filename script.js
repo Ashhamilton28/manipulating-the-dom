93f47b3710547f009aa7875e84f43ffe62e479c5
Ashley <Ashhamilton22888@gmail.com>
2023-02-23 22:13:05 -0500

    im up to part 9

--- script.js
+++ script.js
@@ -29,22 +29,54 @@ document.addEventListener("DOMContentLoaded", function (event) {
   // Part 3
   // let domsFavoriteThings = document.querySelector('ul.last-child')
   let domsFavoriteThings = document.getElementById('favorite-things');
- 
-  domsFavoriteThings.removeChild(domsFavoriteThings.lastElementChild)
- console.log(domsFavoriteThings)
-  // Part 4
 
+  domsFavoriteThings.removeChild(domsFavoriteThings.lastElementChild)
+  console.log(domsFavoriteThings)
+
+  // Part 4
+  let specialTitle = document.querySelectorAll('.special-title')
+  console.log(specialTitle)
+  specialTitle.forEach(element => {
+    element.style.fontSize = '2rem'
+  })
 
   // Part 5
+  let pastRaces = document.getElementById("past-races")
+  let chi = pastRaces.children[3].remove
+  console.log(chi)
 
 
   // Part 6
-
+  let newLiForPastRaces = document.createElement('li')
+  pastRaces.append(newLiForPastRaces)
+  newLiForPastRaces.innerText = "NYC"
 
   // Part 7
+  let mainDivWithBlogPosts = document.querySelector(".main")
+  console.log(mainDivWithBlogPosts)
+  const newDivForBlogPost = document.createElement('div')
+  console.log(newDivForBlogPost)
+  mainDivWithBlogPosts.appendChild(newDivForBlogPost)
+  newDivForBlogPost.className = ".blog-post"
+  const newH2ForBlogPost = document.createElement('h2')
+  // console.log(newH2ForBlogPost)
+  newDivForBlogPost.appendChild(newH2ForBlogPost)
+  newH2ForBlogPost.innerHTML = "NYC"
+  const newPForBlogPost = document.createElement('p')
+  // console.log(newPForBlogPost)
+  newDivForBlogPost.appendChild(newPForBlogPost)
+  newPForBlogPost.innerHTML = "Drifted through the concrete jungle."
+
 
 
   // Part 8
+  // Query select the #quote-titleID element and add a click event handler. That event handler should use the function randomQuote whenever #quote-title is clicked.
+  let quoteOfTheDay = document.querySelector("#quote-of-the-day")
+  console.log(quoteOfTheDay)
+  quoteOfTheDay.addEventListener('click',function(){
+    console.log('clicked')
+  })
+
 
 
   // Part 9
